13f7b39f30eada89de19e279bdccc316f3c44c87
kiingdavies <daviesadedeji007@gmail.com>
2020-08-05 12:44:06 +0100

    second commit

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace pluralrdemo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(" Tek-ninja is the best");
                 });
             });
         }
